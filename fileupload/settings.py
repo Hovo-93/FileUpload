6d08ee4cc74d27ddb1553a00e20e819c83a98f32
Hovo-93 <69504228+Hovo-93@users.noreply.github.com>
2023-09-22 20:21:34 +0300

    added postgresql settings

--- fileupload/settings.py
+++ fileupload/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'fileupload.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'USER': os.getenv('DB_USER'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+
     }
 }
 
